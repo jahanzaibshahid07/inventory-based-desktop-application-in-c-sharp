4fca6663c2bc29e1bb92b23e226ec946676a358d
jahanzaibshahid <jahanzaibshahid07@gmail.com>
2020-08-01 01:59:31 +0500

    first commit

---
+++ WindowsFormsApplication15/WindowsFormsApplication15/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            DAL dal = new DAL();
+            bool check = dal.login(textBox1.Text, textBox2.Text);
+            if (check == true)
+            {
+
+                progressBar1.Show();
+                progressBar1.Minimum = 0;
+                progressBar1.Maximum = 200000;
+                for (int i = 0; i <= 200000; i++)
+                {
+                    progressBar1.Value = i;
+                }
+                this.Hide();
+                Form2 f2 = new Form2();
+                f2.Show();
+            }
+            else 
+            {
+                MessageBox.Show("Invalid User ID and or Password....!!!");
+                textBox1.Clear();
+                textBox2.Clear();
+            
+            }
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            progressBar1.Hide();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ WindowsFormsApplication15/WindowsFormsApplication15/Form2.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WindowsFormsApplication15
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        SqlConnection con = new SqlConnection("Data Source=HP\\SQLEXPRESS;Initial Catalog=inbasemgm;Integrated Security=True");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            con.Close();
+            string s = "select * from products";
+            SqlDataAdapter ad = new SqlDataAdapter(s,con);
+            con.Open();
+            DataTable td = new DataTable();
+            ad.Fill(td);
+            dataGridView1.DataSource = td;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            DAL dal = new DAL();
+            dal.insert(Convert.ToInt32(textBox1.Text), textBox2.Text, Convert.ToInt32(textBox3.Text));
+            MessageBox.Show("Record have been inserted.....");
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DAL dal = new DAL();
+            dal.delete(Convert.ToInt32(textBox4.Text));
+            MessageBox.Show("Record have been Removed.....");
+            textBox4.Clear();
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            DAL dal = new DAL();
+            dal.update(Convert.ToInt32(textBox8.Text), Convert.ToInt32(textBox5.Text), textBox6.Text, Convert.ToInt32(textBox7.Text));
+            MessageBox.Show("Record have been Updated.....");
+            textBox8.Clear();
+            textBox7.Clear();
+            textBox5.Clear();
+            textBox6.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            Form1 f1 = new Form1();
+            f1.Show();
+        }
+    }
+}
